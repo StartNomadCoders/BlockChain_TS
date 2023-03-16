69c67b42decc9b188dcc545baf747d014acdead6
Raon98 <cheol1998@kakao.com>
2023-03-16 19:22:00 +0900

    타입스크립트를 쓰는이유

---
+++ js/Why_TypeScript.js
@@ -0,0 +1,11 @@
+'use strict'
+
+function divide(a,b){
+    return a / b
+}
+console.log(divide(2,3)) //0.666666
+console.log(divide("XXXXXX")) //NaN
+
+const nico = { name : "nico"}
+
+nico.hello();
--- js/hello.js
+++ js/hello.js
@@ -1,1 +1,8 @@
-console.log("hello");
+"use strict"
+let a = [1, 2, 3, 4, 5];
+let f = a.reduce((a, b) => {
+    (b % 2) ? a.push(b) : a
+    return a
+},[]);
+
+console.log("f값 확인" + f);
