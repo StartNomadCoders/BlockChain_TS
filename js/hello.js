0cd0549dea837c0b811254187ac26c132e1be9d8
Raon98 <cheol1998@kakao.com>
2023-03-21 17:41:35 +0900

    js : promise Ex

--- js/hello.js
+++ js/hello.js
@@ -1,8 +1,18 @@
 "use strict"
-let a = [1, 2, 3, 4, 5];
-let f = a.reduce((a, b) => {
-    (b % 2) ? a.push(b) : a
-    return a
-},[]);
+// let a = [1, 2, 3, 4, 5];
+// let f = a.reduce((a, b) => {
+//     (b % 2) ? a.push(b) : a
+//     return a
+// },[]);
+//
+// console.log("f값 확인" + f);
 
-console.log("f값 확인" + f);
+let inputA = 5
+const resolveAfterNSconds = (n) => {
+    return new Promise((res,_) => setTimeout(()=> res(`${n}초가 지났습니다.`),n*1000))
+}
+
+resolveAfterNSconds(inputA).then((text) => {
+   console.log(text)
+});
+setTimeout(() => console.log("2초 후에 실행됨"), 2000)
