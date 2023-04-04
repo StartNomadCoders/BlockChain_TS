1959287327efe8e4f36244bae125048209cf57f2
Raon98 <cheol1998@kakao.com>
2023-04-04 19:18:08 +0900

    20230404 JSDoc

--- src/myPackage.js
+++ src/myPackage.js
@@ -1,6 +1,20 @@
+// @ts-check
+/** JSDoc
+ * Initialize the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns
+ */
 export function init(config){
     return true
 }
+
+/**
+ *Exits the program
+ * @param {number} code
+ * @return
+ */
 export function exit(code){
     return code + 1;
 }
